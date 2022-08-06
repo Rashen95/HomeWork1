c3871c4aceb9d954d2a82ace61df51e2f2a55f11
Artem Privalov <rashen95@mail.ru>
2022-08-06 11:46:44 +0300

    Решено четвертое задание

---
+++ Exercise4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Эта программа для вывода всех четных чисел от 1 вплоть до введенного в консоль");
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int number = 2;
+if (a <= 1)
+{
+    Console.WriteLine("Введенное число меньше или равно 1!");
+}
+else
+{   
+    Console.Write("Все четные числа до введенного включительно: ");
+    while (number < a - 1)
+    {
+        Console.Write(number + ", ");
+        number += 2;
+    }
+Console.WriteLine(number);
+}
