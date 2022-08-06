a18fec239a3d3aa5c0799b2ff01f0bbec6d1dbaf
Artem Privalov <rashen95@mail.ru>
2022-08-06 10:12:13 +0300

    Решено второе задание

---
+++ Exercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Это программа для нахождения максимального числа из трех введенных");
+Console.WriteLine("ВАЖНО!!! При необходимости сравнения чисел с плавающей точкой в графу ввода нужно вводить число через запятую");
+Console.Write("Введите первое число: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double c = Convert.ToDouble(Console.ReadLine());
+double max = a;
+if (a == b && b == c) Console.WriteLine("Все три числа равны!");
+else
+{
+    if (b > max) max = b;
+    if (c > max) max = c;
+    Console.WriteLine("Максимальное число равно " + max);
+}
