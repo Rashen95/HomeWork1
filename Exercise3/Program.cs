2316f4a4a252662e246472ea42e69a16726606ee
Artem Privalov <rashen95@mail.ru>
2022-08-06 10:24:57 +0300

    Решено третье задание

---
+++ Exercise3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Эта программа для проверки четности введенного числа");
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0) 
+{
+    Console.WriteLine(a + " является четным числом");
+}
+else
+{
+    Console.WriteLine(a + " является нечетным числом");
+}
